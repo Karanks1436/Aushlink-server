61288252c241d1baf497ffb1ae49ac44d69ff7b9
Karan Kumar <karanksxxx@gmail.com>
2025-08-01 00:11:57 +0530

    update

--- controller/aadhaarController.js
+++ controller/aadhaarController.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const { GoogleGenerativeAI } = require('@google/generative-ai');
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-exports.aadhaarVerify = async (req, res) => {
+exports.aadhaarVerify = async (req, res) => { 
   try {
     const file = req.file;
     const side = req.body.side;
